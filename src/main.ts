ed21dbc40feedd5328241558b43315fbd89a7230
Fayaz Shaik <shaikfaizu@gmail.com>
2024-09-19 16:44:37 +0530

    bootstrap issue resolved

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
